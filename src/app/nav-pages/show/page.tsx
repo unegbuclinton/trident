f37e991521cadd61d64dd5868f7f2aa3e82cd106
unegbu Clinton <unegbuclinton5@gmail.com>
2024-08-01 12:22:10 +0100

    update UI

--- src/app/nav-pages/show/page.tsx
+++ src/app/nav-pages/show/page.tsx
@@ -4,17 +4,17 @@ const Show = () => {
   return (
     <div className='py-36 lg:py-20 text-white max-w-[757px] mx-auto my-0 text-xl font-light lato'>
       <h2 className='mb-14 text-[40px] lato text-secondaryColor'>The show</h2>
-      <p className='pb-4'>
+      <p className='pb-4 leading-10 text-lg font-light'>
         TCT Nigeria is presenting an adversarial style tv variety and reality
         show that promises the winners $50, 000 in prize money. 
       </p>
-      <p className='pb-4'>
+      <p className='pb-4 leading-10 text-lg font-light'>
         TCT Nigeria presents BetrayAll where 22 contestants will fight to emerge
         the winner of $50, 000 while putting friendships, alliances and
         relationships to the test in a winner takes all contest, are you brave
         enough to go for it? Show us now.
       </p>
-      <p className='pb-4'>
+      <p className='pb-4 leading-10 text-lg font-light'>
         Enter to be one of twenty two people who will make it to the campus to
         vie against the system, and other contestants for a shot at glory, and
         money!
