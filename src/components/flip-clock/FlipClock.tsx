c81d6baaa211abffdb939c19735c40b991f43108
unegbu Clinton <unegbuclinton5@gmail.com>
2024-08-08 12:50:30 +0100

    extract countdown clock

--- src/components/flip-clock/FlipClock.tsx
+++ src/components/flip-clock/FlipClock.tsx
@@ -1,6 +1,6 @@
 'use client'
 import useCountdownTimer from '@/lib/conutdownTimer'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 const FlipClock = () => {
   const preciseEndDate = new Date('2024-08-26T00:00:00').getTime()
